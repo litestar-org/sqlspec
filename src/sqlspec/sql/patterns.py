c2843b78eb65c47461db48043202fe90bd4bf536
Cody Fincher <cody.fincher@gmail.com>
2024-06-07 21:12:23 +0000

    feat: linting

--- src/sqlspec/sql/patterns.py
+++ src/sqlspec/sql/patterns.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from sqlspec.types.protocols import SQLOperationType
 
 try:
-    import re2 as re  # pyright: ignore[reportMissingImports]
+    import re2 as re  # pyright: ignore[reportMissingImports,reportMissingTypeStubs]
 except ImportError:
     import re
 
 
 # FIXME to be improved
-VAR_REF = re.compile(
+VAR_REF = re.compile(  # pyright: ignore[reportUnknownMemberType]
     r'(?P<dquote>"(""|[^"])+")|'  # Double-quoted strings
     r"(?P<squote>'(''|[^'])*')|"  # Single-quoted strings
     r"(?P<lead>[^:]):(?P<var_name>\w+)"  # Colon-variables
@@ -17,20 +17,20 @@ VAR_REF = re.compile(
 """Pattern to identify colon-variables (aka _named_ style) in SQL code"""
 
 
-QUERY_DEF = re.compile(r"--\s*name\s*:\s*")
+QUERY_DEF = re.compile(r"--\s*name\s*:\s*")  # pyright: ignore[reportUnknownMemberType]
 """Identifies name definition comments"""
 
-QUERY_RECORD_DEF = re.compile(r"--\s*record_class\s*:\s*(\w+)\s*")
+QUERY_RECORD_DEF = re.compile(r"--\s*record_class\s*:\s*(\w+)\s*")  # pyright: ignore[reportUnknownMemberType]
 """ identifies record class definition comments"""
 # FIXME this accepts "1st" but seems to reject "é"
-QUERY_OPERATION_NAME = re.compile(r"^(\w+)(|\^|\$|!|<!|\*!|#)$")
+QUERY_OPERATION_NAME = re.compile(r"^(\w+)(|\^|\$|!|<!|\*!|#)$")  # pyright: ignore[reportUnknownMemberType]
 """extract a valid query name followed by an optional operation spec"""
 
 
-BAD_PREFIX = re.compile(r"^\d")
+BAD_PREFIX = re.compile(r"^\d")  # pyright: ignore[reportUnknownMemberType]
 """forbid numbers as first character"""
 
-SQL_COMMENT = re.compile(r"\s*--\s*(.*)$")
+SQL_COMMENT = re.compile(r"\s*--\s*(.*)$")  # pyright: ignore[reportUnknownMemberType]
 """Get SQL comment contents"""
 
 SQL_OPERATION_TYPES = {
@@ -45,7 +45,7 @@ SQL_OPERATION_TYPES = {
 }
 """map operation suffixes to their type"""
 
-UNCOMMENT = re.compile(
+UNCOMMENT = re.compile(  # pyright: ignore[reportUnknownMemberType]
     r"(?P<squote>'(?:''|[^'])*')"  # Single-quoted strings
     r"|(?P<dquote>\"(?:[^\"]|\"\")*\")"  # Double-quoted strings
     r"|(?P<oneline>--[^\n]*)"  # One-line comments
