6c099331dc2f3c85144b07e267fe1753403a5420
Cody Fincher <cody@litestar.dev>
2025-04-20 23:48:31 +0000

    feat: adds a `bigquery` config and driver

---
+++ sqlspec/adapters/bigquery/__init__.py
@@ -0,0 +1,4 @@
+from sqlspec.adapters.bigquery.config import BigQueryConfig, BigQueryConnectionConfig
+from sqlspec.adapters.bigquery.driver import BigQueryDriver
+
+__all__ = ("BigQueryConfig", "BigQueryConnectionConfig", "BigQueryDriver")
