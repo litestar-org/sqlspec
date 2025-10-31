c42b25c69716be44953504d4816db11757037296
euri10 <benoit.barthelet@gmail.com>
2025-10-31 08:03:18 +0100

    3

--- docs/examples/usage/test_index_3.py
+++ docs/examples/usage/test_index_3.py
@@ -7,7 +7,7 @@ from sqlspec.loader import SQLFileLoader
 spec = SQLSpec()
 db = spec.add_config(SqliteConfig())
 with spec.provide_session(db) as session:
-    _   = session.execute("""
+    _ = session.execute("""
     CREATE TABLE users (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
@@ -15,15 +15,16 @@ with spec.provide_session(db) as session:
         active BOOLEAN NOT NULL DEFAULT 1
     )
     """)
-    _   = session.execute("""INSERT INTO users VALUES (1, 'alice', 'alice
+    _ = session.execute("""INSERT INTO users VALUES (1, 'alice', 'alice
 @example.com', 1),
                            (2, 'bob', 'bob@example.com', 0),
                            (3, 'carol', 'carol@examplecom', 1)""")
 
 
-
+p = Path(__file__).parent.parent / "queries/users.sql"
+assert p.exists()
 loader = SQLFileLoader()
-loader.load_sql(Path(__file__) / "queries/users.sql")
+loader.load_sql(p)
 
 user_query = loader.get_sql("get_user_by_id", user_id=2)
 result = session.execute(user_query)
