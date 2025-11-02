cbbb7ebbecfd75654e11fe70c1bc7a0e8faee445
euri10 <benoit.barthelet@gmail.com>
2025-11-02 14:55:10 +0100

    Update SQLite example to set defaults for created_at and updated_at fields to CURRENT_TIMESTAMP
    Signed-off-by: euri10 <benoit.barthelet@gmail.com>

--- docs/examples/usage/test_index_3.py
+++ docs/examples/usage/test_index_3.py
@@ -10,12 +10,14 @@ with spec.provide_session(db) as session:
     _ = session.execute("""
     CREATE TABLE users (
         id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
+        username TEXT NOT NULL,
         email TEXT,
-        active BOOLEAN NOT NULL DEFAULT 1
+        active BOOLEAN NOT NULL DEFAULT 1,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
-    _ = session.execute("""INSERT INTO users VALUES (1, 'alice', 'alice
+    _ = session.execute("""INSERT INTO users(id, username, email, active) VALUES (1, 'alice', 'alice
 @example.com', 1),
                            (2, 'bob', 'bob@example.com', 0),
                            (3, 'carol', 'carol@examplecom', 1)""")
@@ -26,8 +28,8 @@ assert p.exists()
 loader = SQLFileLoader()
 loader.load_sql(p)
 
-user_query = loader.get_sql("get_user_by_id", user_id=2)
-result = session.execute(user_query)
+user_query = loader.get_sql("get_user_by_id")
+result = session.execute(user_query, user_id=2)
 
 
 def test_index_3() -> None:
