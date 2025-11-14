320b43283fa250d1279da166ae8f9aaef3550b1a
Cody Fincher <cody@litestar.dev>
2025-11-14 18:59:27 +0000

    fix: mark the mixin file as private like the others

--- sqlspec/driver/mixins/__init__.py
+++ sqlspec/driver/mixins/__init__.py
@@ -2,6 +2,6 @@
 
 from sqlspec.driver.mixins._result_tools import ToSchemaMixin
 from sqlspec.driver.mixins._sql_translator import SQLTranslatorMixin
-from sqlspec.driver.mixins.storage import StorageDriverMixin
+from sqlspec.driver.mixins._storage import StorageDriverMixin
 
 __all__ = ("SQLTranslatorMixin", "StorageDriverMixin", "ToSchemaMixin")
