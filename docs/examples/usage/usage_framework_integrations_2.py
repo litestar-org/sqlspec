ff6c9a92c7ac6cf0a4bf75953e92ef42b9c9e85a
euri10 <benoit.barthelet@gmail.com>
2025-11-19 06:36:28 +0100

    fix test 2

--- docs/examples/usage/usage_framework_integrations_2.py
+++ docs/examples/usage/usage_framework_integrations_2.py
@@ -1,7 +1,6 @@
 # start-example
 from typing import Any
 
-import pytest
 from asyncpg import Connection, Pool
 from litestar import Litestar, get
 from litestar.testing import AsyncTestClient
@@ -18,7 +17,7 @@ __all__ = ("client", "get_user", "health_check", "stats", "test_stub")
 # Inject database session
 @get("/users/{user_id:int}")
 async def get_user(user_id: int, db_session: AsyncDriverAdapterBase) -> dict:
-    result = await db_session.execute("SELECT id, name, email FROM users WHERE id = $1", user_id)
+    result = await db_session.execute("SELECT id, name, email FROM users_fi2 WHERE id = $1", user_id)
     return result.one()
 
 
@@ -33,13 +32,14 @@ async def health_check(db_pool: Pool) -> dict[str, Any]:
 # Inject raw connection
 @get("/stats")
 async def stats(db_connection: Connection) -> dict[str, Any]:
-    result = await db_connection.fetchval("SELECT COUNT(*) FROM users")
+    result = await db_connection.fetchval("SELECT COUNT(*) FROM users_fi2")
     return {"user_count": result}
 
 
 # end-example
-@pytest.fixture
-async def client(postgres_service: PostgresService):
+
+
+async def test_stub(postgres_service: PostgresService) -> None:
     spec = SQLSpec()
     config = AsyncpgConfig(
         pool_config={
@@ -53,11 +53,14 @@ async def client(postgres_service: PostgresService):
     spec.add_config(config)
 
     sqlspec_plugin = SQLSpecPlugin(sqlspec=spec)
+    async with spec.provide_session(config) as session:
+        await session.execute("""CREATE TABLE users_fi2(id integer primary key, name text, email text)""")
+        await session.execute("""insert into users_fi2(id, name, email) values (1, 'Alice', 'alice@example.com')""")
     app = Litestar(route_handlers=[health_check, stats, get_user], plugins=[sqlspec_plugin], debug=True)
     async with AsyncTestClient(app) as client:
-        yield client
-
-
-async def test_stub(client: AsyncTestClient) -> None:
-    response = await client.get("/stats")
-    assert response == {"status": "healthy"}
+        response = await client.get("/health")
+        assert response.json() == {"status": "healthy"}
+        response = await client.get("/stats")
+        assert response.json() == {"user_count": 1}
+        response = await client.get("/users/1")
+        assert response.json() == {"email": "alice@example.com", "id": 1, "name": "Alice"}
