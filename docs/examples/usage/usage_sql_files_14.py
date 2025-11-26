d02875d28b890b199102ecc1e2d1689fc080d833
euri10 <benoit.barthelet@gmail.com>
2025-11-26 12:13:13 +0100

    unique db

--- docs/examples/usage/usage_sql_files_14.py
+++ docs/examples/usage/usage_sql_files_14.py
@@ -15,7 +15,7 @@ async def test_multi_database_setup_example(tmp_path: Path, postgres_service: Po
     user_sql_file_pg = user_sql_path_pg / "users.sql"
     user_sql_file_pg.write_text(
         """-- name: upsert_user
-        INSERT INTO users (id, username, email) VALUES (:id, :username, :email)
+        INSERT INTO users_sf1 (id, username, email) VALUES (:id, :username, :email)
         ON CONFLICT (id) DO UPDATE SET username = EXCLUDED.username, email = EXCLUDED.email;
         """
     )
@@ -24,7 +24,7 @@ async def test_multi_database_setup_example(tmp_path: Path, postgres_service: Po
     user_sql_file_sqlite = user_sql_path_sqlite / "users.sql"
     user_sql_file_sqlite.write_text(
         """-- name: get_user
-        SELECT id, username, email FROM users WHERE id = :user_id;
+        SELECT id, username, email FROM users_sf1 WHERE id = :user_id;
         """
     )
     shared_sql_path = tmp_path / "sql" / "shared"
@@ -32,7 +32,7 @@ async def test_multi_database_setup_example(tmp_path: Path, postgres_service: Po
     shared_sql_file = shared_sql_path / "common.sql"
     shared_sql_file.write_text(
         """-- name: delete_user
-        DELETE FROM users WHERE id = :user_id;
+        DELETE FROM users_sf1 WHERE id = :user_id;
         """
     )
     params = {"id": 1, "username": "john_doe", "email": "jd@example.com"}
@@ -61,16 +61,18 @@ async def test_multi_database_setup_example(tmp_path: Path, postgres_service: Po
     sqlite_config = SqliteConfig()
     # Execute on appropriate database
     async with spec.provide_session(postgres_config) as pg_session:
-        await pg_session.execute("""CREATE TABLE users ( id INTEGER PRIMARY KEY, username TEXT, email TEXT)""")
+        await pg_session.execute("""CREATE TABLE users_sf1 ( id INTEGER PRIMARY KEY, username TEXT, email TEXT)""")
         await pg_session.execute(
-            """ INSERT INTO users (id, username, email) VALUES (1, 'old_name', 'old@example.com');"""
+            """ INSERT INTO users_sf1 (id, username, email) VALUES (1, 'old_name', 'old@example.com');"""
         )
 
         await pg_session.execute(pg_query, **params)
 
     with spec.provide_session(sqlite_config) as sqlite_session:
-        sqlite_session.execute("""CREATE TABLE users ( id INTEGER PRIMARY KEY, username TEXT, email TEXT)""")
-        sqlite_session.execute(""" INSERT INTO users (id, username, email) VALUES (1, 'john_doe', 'jd@example.com');""")
+        sqlite_session.execute("""CREATE TABLE users_sf1 ( id INTEGER PRIMARY KEY, username TEXT, email TEXT)""")
+        sqlite_session.execute(
+            """ INSERT INTO users_sf1 (id, username, email) VALUES (1, 'john_doe', 'jd@example.com');"""
+        )
         sqlite_session.execute(sqlite_query, user_id=1)
     # end-example
     # Dummy asserts for doc example
