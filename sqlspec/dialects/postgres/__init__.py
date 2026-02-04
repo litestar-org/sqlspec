ccebe5fe80b07ce64c38389b7f63b6b340ff70f3
Jordan Russell <jordan.russell@sitehost.co.nz>
2026-02-05 07:45:35 +1300

    feat(dialects): add sqlglot dialect extensions for PGVector and ParadeDB
    Adds dialect extensions for PGVector and ParadeDB. This uses a
    workaround for missing Tokens by 'sharing' unused Postgres tokens. New
    extension tokens won't be upstreamed as per
    https://github.com/tobymao/sqlglot/issues/6949

--- sqlspec/dialects/postgres/__init__.py
+++ sqlspec/dialects/postgres/__init__.py
@@ -3,4 +3,7 @@
 Landing zone for PGVector, ParadeDB, and other Postgres extension dialects.
 """
 
-__all__: tuple[str, ...] = ()
+from sqlspec.dialects.postgres.paradedb import ParadeDB
+from sqlspec.dialects.postgres.pgvector import PGVector
+
+__all__: tuple[str, ...] = ("PGVector", "ParadeDB")
